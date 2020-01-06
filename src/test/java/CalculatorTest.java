8b676744d74717af19ea403aff91d4fa907d01c8
Eduardo Gizmo <eduardogimenezmoreno@gmail.com>
2020-01-06 15:08:21 +0000

    Added Add functionality

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,2 +1,13 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class CalculatorTest {
+
+    @Test
+    public void shouldAddTwoNumbers(){
+        Calculator  calculator = new Calculator();
+        assertEquals(20.00, calculator.add(10.00, 10.00), 0.01);
+    }
+
 }
