481c90207653e2105229225e56c9ecea50bd3ea7
Eduardo Gizmo <eduardogimenezmoreno@gmail.com>
2020-01-06 15:35:57 +0000

    Bottle should start with a volume of 100

--- src/main/java/Bottle.java
+++ src/main/java/Bottle.java
@@ -1,4 +1,16 @@
 public class Bottle {
 
+    private int volume;
 
+    public Bottle() {
+        this.volume = 100;
+    }
+
+//    public Bottle(int volume) {
+//        this.volume = volume;
+//    }
+
+    public int getVolume(){
+        return this.volume;
+    }
 }
--- src/test/java/BottleTest.java
+++ src/test/java/BottleTest.java
@@ -1,5 +1,20 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class BottleTest {
 
+    private Bottle bottle;
 
+    @Before
+    public void init(){
+        bottle = new Bottle();
+    }
+
+    @Test
+    public void shouldStartsAtVolume100(){
+        assertEquals(100, bottle.getVolume());
+    }
 
 }
