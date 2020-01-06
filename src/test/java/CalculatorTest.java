a8d76f08334385278702ffb968059a3a425fe6d5
Eduardo Gizmo <eduardogimenezmoreno@gmail.com>
2020-01-06 15:16:25 +0000

    Added Subtract functionality

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,13 +1,23 @@
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class CalculatorTest {
 
-    @Test
-    public void shouldAddTwoNumbers(){
-        Calculator  calculator = new Calculator();
-        assertEquals(20.00, calculator.add(10.00, 10.00), 0.01);
+    private Calculator calculator;
+
+    @Before
+    public void init(){
+        calculator = new Calculator();
     }
 
+    @Test
+    public void shouldAddTwoNumbers(){
+        assertEquals(20.00, calculator.add(10.00, 10.00), 0.01);
+    }
+    @Test
+    public void shouldSubtractTwoNumbers(){
+        assertEquals(10.00, calculator.subtract(20.00, 10.00), 0.01);
+    }
 }
