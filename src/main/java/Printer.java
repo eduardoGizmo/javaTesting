5df317d556988a8aa54d0df6ec0a7f9f213afc40
Eduardo Gizmo <eduardogimenezmoreno@gmail.com>
2020-01-06 20:19:11 +0000

    Added getToner functionality and Reduce toner by 1 when printing functionality

--- src/main/java/Printer.java
+++ src/main/java/Printer.java
@@ -1,13 +1,26 @@
 public class Printer {
 
     private int sheets;
+    private int toner;
 
-    public Printer(int sheets) {
+    public Printer(int sheets, int toner) {
         this.sheets = sheets;
+        this.toner = toner;
     }
 
     public int getSheets(){
         return this.sheets;
     }
 
+    public int getToner(){
+        return this.toner;
+    }
+
+    public void print(int pages, int copies ){
+        int sheetsToPrint = pages * copies;
+        if (this.sheets >= sheetsToPrint) {
+            this.sheets -= sheetsToPrint;
+            this.toner -= sheetsToPrint;
+        }
+    }
 }
--- src/test/java/PrinterTest.java
+++ src/test/java/PrinterTest.java
@@ -9,11 +9,29 @@ public class PrinterTest {
 
     @Before
     public void init(){
-        printer = new Printer(90);
+        printer = new Printer(90, 200);
     }
 
     @Test
     public void shouldCountNumOfSheets(){
+        assertEquals(90, printer.getSheets());
+    }
+
+    @Test
+    public void shouldCountTonerVolume(){
+        assertEquals( 200, printer.getToner());
+    }
+
+    @Test
+    public void shouldPrint(){
+        printer.print(10, 4);
+        assertEquals(50, printer.getSheets());
+        assertEquals(160, printer.getToner());
+    }
+
+    @Test
+    public void shouldntPrint(){
+        printer.print(10, 10);
         assertEquals(90, printer.getSheets());
     }
 
